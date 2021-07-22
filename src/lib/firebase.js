34b2e5695f74f16654a3a7fdb4e35a073f07c802
Camerone Stoney <Cameronestoney@yahoo.com>
2021-07-21 21:12:51 -0700

    chore: config updates

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,14 +5,15 @@ import 'firebase/firestore';
 
 // Initalize Firebase.
 const firebaseConfig = {
-  apiKey: "AIzaSyDlgVTZkjq6HT0pFm7sBiSm60EQ19D2RrI",
-  authDomain: "tcl-26-shopping-list.firebaseapp.com",
-  projectId: "tcl-26-shopping-list",
-  storageBucket: "tcl-26-shopping-list.appspot.com",
-  messagingSenderId: "368940430821",
-  appId: "1:368940430821:web:5347ad9d833f3a84cd6b0c"
+  apiKey: 'AIzaSyAi9oBP-hXAU43ubGEkmF3eXmWl48dNVK0',
+  authDomain: 'smart-list-cs.firebaseapp.com',
+  projectId: 'smart-list-cs',
+  storageBucket: 'smart-list-cs.appspot.com',
+  messagingSenderId: '24601715025',
+  appId: '1:24601715025:web:037051b48a46727f657370',
+  measurementId: 'G-MXNYC50JM5',
 };
 
-let fb = firebase.initializeApp(firebaseConfig);
-
-export { fb };
+let firebaseInstance = firebase.initializeApp(firebaseConfig);
+const db = firebaseInstance.firestore();
+export { db };
