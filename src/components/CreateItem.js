04c8182076506f7ce1151f83e4e41c49545f7f0b
Camerone Stoney <Cameronestoney@yahoo.com>
2021-07-21 21:11:56 -0700

    refactor: Added the two components add/view

---
+++ src/components/CreateItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { db } from '../lib/firebase';
+
+const CreateItem = () => {
+  async function handleClick(e) {
+    await db.collection('products').add({
+      name: 'NewItem',
+      date: Date.now().toLocaleString('en-US', { timezone: 'PCT' }),
+    });
+  }
+  return <button onClick={handleClick}>Add an Item</button>;
+};
+
+export default CreateItem;
---
+++ src/components/ViewItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { db } from '../lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+const ViewItem = () => {
+  const [listItems, loading, error] = useCollection(db.collection('items'));
+
+  return (
+    <div>
+      {!!loading && <>Loading...</>}
+      {!!error && <> Error... </>}
+      {!loading && !!listItems && (
+        <ul>
+          {listItems.docs.map((doc) => {
+            const { name, createdAt } = doc.data();
+            const date = new Date(createdAt);
+            return (
+              <li key={doc.id}>
+                {`${name} created at ${date.toLocaleString('en-US', {
+                  timeZone: 'PCT',
+                })}`}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ViewItem;
