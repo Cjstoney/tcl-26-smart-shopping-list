281ad5b68a0d263a79cccc828606d8eae3b4488f
Camerone Stoney <Cameronestoney@yahoo.com>
2021-07-23 09:02:00 -0700

    chore: initial instal of react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ViewItem from './components/ViewItem';
 import './App.css';
 import CreateItem from './components/CreateItem';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <ViewItem />
-      <CreateItem />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <ViewItem />
+        </Route>
+        <Route path="/add">
+          <CreateItem />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
